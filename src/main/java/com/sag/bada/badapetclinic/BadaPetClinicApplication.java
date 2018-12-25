46800adbd6d68df0f3a27fc095a92207e2b482d6
Das, Bapi <Bapi.Das@softwareag.com>
2018-12-25 10:59:09 +0530

    Initial project setup for pet clinic

---
+++ src/main/java/com/sag/bada/badapetclinic/BadaPetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.sag.bada.badapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadaPetClinicApplication.class, args);
+	}
+
+}
+
