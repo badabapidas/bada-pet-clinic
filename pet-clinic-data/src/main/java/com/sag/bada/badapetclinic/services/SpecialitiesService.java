8883998cc9548f4011708062f245f741f10763b7
Das, Bapi <Bapi.Das@softwareag.com>
2018-12-29 19:04:37 +0530

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/sag/bada/badapetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.sag.bada.badapetclinic.services;
+
+import com.sag.bada.badapetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
