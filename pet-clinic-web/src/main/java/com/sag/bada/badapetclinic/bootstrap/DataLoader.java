e11c5a3f79dcc4000f0613a4daab4abbe422143c
Das, Bapi <Bapi.Das@softwareag.com>
2018-12-29 20:11:09 +0530

    Modified the data model for clear understanding

--- pet-clinic-web/src/main/java/com/sag/bada/badapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sag/bada/badapetclinic/bootstrap/DataLoader.java
@@ -44,6 +44,24 @@ public class DataLoader implements CommandLineRunner {
 	}
 
 	private void loadData() {
+
+		// Create Owners
+		Owner owner1 = new Owner();
+		owner1.setFirstName("Bapi");
+		owner1.setLastName("Das");
+		owner1.setAddress("Martahhalli");
+		owner1.setCity("Bangalore");
+		owner1.setTelephone("11111111");
+
+		Owner owner2 = new Owner();
+		owner2.setFirstName("Umar");
+		owner2.setLastName("BaBa");
+		owner2.setAddress("Assam");
+		owner1.setCity("Guwahati");
+		owner1.setTelephone("2222222");
+
+		// Create Pet Types which will be needing for Pet- two pet types created Dog,
+		// Cat
 		PetType dog = new PetType();
 		dog.setName("Dog");
 		PetType saveDogPetType = petTypeService.save(dog);
@@ -52,6 +70,27 @@ public class DataLoader implements CommandLineRunner {
 		dog.setName("Cat");
 		PetType saveCatPetType = petTypeService.save(cat);
 
+		// Create Pets for Owner Bapi and Umar
+		Pet bapisPet = new Pet();
+		bapisPet.setPetType(saveDogPetType);
+		bapisPet.setOwner(owner1);
+		bapisPet.setBirthDate(LocalDate.now());
+		bapisPet.setName("Rosco");
+		owner1.getPets().add(bapisPet);
+
+		Pet umarsPet = new Pet();
+		umarsPet.setPetType(saveCatPetType);
+		umarsPet.setOwner(owner2);
+		umarsPet.setBirthDate(LocalDate.now());
+		umarsPet.setName("Batra");
+		owner2.getPets().add(umarsPet);
+
+		// save the owner instances
+		ownerService.save(owner1);
+		ownerService.save(owner2);
+		System.out.println("Loaded Owners....");
+
+		// Create 3 specialities for Vets
 		Speciality radiology = new Speciality();
 		radiology.setDescription("Radiology");
 		Speciality savedRadiology = specialtiesService.save(radiology);
@@ -64,54 +103,19 @@ public class DataLoader implements CommandLineRunner {
 		dentistry.setDescription("Dentistry");
 		Speciality savedDentistry = specialtiesService.save(dentistry);
 
-		Owner owner1 = new Owner();
-		owner1.setFirstName("Bapi");
-		owner1.setLastName("Das");
-		owner1.setAddress("Martahhalli");
-		owner1.setCity("Bangalore");
-		owner1.setTelephone("11111111");
-
-		Pet bapisPet = new Pet();
-		bapisPet.setPetType(saveDogPetType);
-		bapisPet.setOwner(owner1);
-		bapisPet.setBirthDate(LocalDate.now());
-		bapisPet.setName("Rosco");
-		owner1.getPets().add(bapisPet);
-
-		ownerService.save(owner1);
-
-		Owner owner2 = new Owner();
-		owner2.setFirstName("Umar");
-		owner2.setLastName("BaBa");
-		owner2.setAddress("Assam");
-		owner1.setCity("Guwahati");
-		owner1.setTelephone("2222222");
-
-		Pet umarsPet = new Pet();
-		umarsPet.setPetType(saveCatPetType);
-		umarsPet.setOwner(owner2);
-		umarsPet.setBirthDate(LocalDate.now());
-		umarsPet.setName("Batra");
-		owner2.getPets().add(umarsPet);
-
-		ownerService.save(owner2);
-
-		System.out.println("Loaded Owners....");
-
+		// Creates Vets
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Sandeep");
 		vet1.setLastName("Sharma");
 		vet1.getSpecialities().add(savedRadiology);
-
-		vetService.save(vet1);
 
 		Vet vet2 = new Vet();
 		vet2.setFirstName("Anuj");
 		vet2.setLastName("Baruah");
 		vet2.getSpecialities().add(savedSurgery);
 
+		vetService.save(vet1);
 		vetService.save(vet2);
-
 		System.out.println("Loaded Vets....");
 	}
 
