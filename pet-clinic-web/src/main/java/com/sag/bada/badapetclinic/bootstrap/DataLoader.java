52b8cf1370a57d13358cb2fedf239aacc6f58f47
Das, Bapi <Bapi.Das@softwareag.com>
2018-12-29 15:07:01 +0530

    Added PetType to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/com/sag/bada/badapetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sag/bada/badapetclinic/bootstrap/DataLoader.java
@@ -5,26 +5,38 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.sag.bada.badapetclinic.model.Owner;
+import com.sag.bada.badapetclinic.model.PetType;
 import com.sag.bada.badapetclinic.model.Vet;
 import com.sag.bada.badapetclinic.services.OwnerService;
+import com.sag.bada.badapetclinic.services.PetTypeService;
 import com.sag.bada.badapetclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
+	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
 	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		super();
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
-
-	private final VetService vetService;
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType saveDogPetType = petTypeService.save(dog);
+
+		PetType cat = new PetType();
+		dog.setName("Cat");
+		PetType saveCatPetType = petTypeService.save(cat);
+
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Bapi");
 		owner1.setLastName("Das");
