a344d9c330ed393cf99245938b54e19a201ef4e2
Das, Bapi <Bapi.Das@softwareag.com>
2018-12-30 23:56:48 +0530

    Implemented Base Entity. Closes #35

--- pet-clinic-data/src/main/java/com/sag/bada/badapetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sag/bada/badapetclinic/model/BaseEntity.java
@@ -2,12 +2,21 @@ package com.sag.bada.badapetclinic.model;
 
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable{
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -7197452314090512613L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
