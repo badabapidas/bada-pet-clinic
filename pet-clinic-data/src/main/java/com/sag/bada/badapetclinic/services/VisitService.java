f16770321753c62bbee979c7765e63e428a65bf5
Das, Bapi <Bapi.Das@softwareag.com>
2018-12-31 10:42:48 +0530

    Added a Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/sag/bada/badapetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.sag.bada.badapetclinic.services;
+
+import com.sag.bada.badapetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
--- pet-clinic-data/src/main/java/com/sag/bada/badapetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/sag/bada/badapetclinic/services/map/PetMapService.java
@@ -8,7 +8,7 @@ import com.sag.bada.badapetclinic.model.Pet;
 import com.sag.bada.badapetclinic.services.PetService;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
 	@Override
 	public Set<Pet> findAll() {
